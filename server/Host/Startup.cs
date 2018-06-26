ffa67d282de954f9e071a6c6c355883a39a24d45
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2018-06-26 14:26:47 +0430

    configure asp.net core webapi

--- server/Host/Startup.cs
+++ server/Host/Startup.cs
@@ -11,13 +11,11 @@ namespace Brainvest.Dscribe.Host
 {
 	public class Startup
 	{
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
 			if (env.IsDevelopment())
@@ -25,10 +23,8 @@ namespace Brainvest.Dscribe.Host
 				app.UseDeveloperExceptionPage();
 			}
 
-			app.Run(async (context) =>
-			{
-				await context.Response.WriteAsync("Hello World!");
-			});
+			app.UseHttpsRedirection();
+			app.UseMvc();
 		}
 	}
 }
