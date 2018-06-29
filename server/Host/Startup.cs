374d440a2e30d5d921b9398016488b9d58f63d36
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2018-06-29 11:54:18 +0430

    setup Host

--- server/Host/Startup.cs
+++ server/Host/Startup.cs
@@ -2,17 +2,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Brainvest.Dscribe.MetadataDbAccess;
+using Brainvest.Dscribe.Runtime;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Brainvest.Dscribe.Host
 {
 	public class Startup
 	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddCors(options => options.AddPolicy("AllowAll",
+				builder =>
+				builder
+					.AllowAnyMethod()
+					.AllowAnyOrigin()
+					.AllowAnyHeader()));
+
+			services.AddDbContext<MetadataDbContext>(options =>
+					options.UseSqlServer(Configuration.GetConnectionString("EngineConnectionString")));
+
+			RuntimeStartup.ConfigureServices(services, Configuration);
+
 			services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
 		}
 
@@ -22,8 +45,8 @@ namespace Brainvest.Dscribe.Host
 			{
 				app.UseDeveloperExceptionPage();
 			}
-
-			app.UseHttpsRedirection();
+			app.UseCors("AllowAll");
+			RuntimeStartup.Configure(app, env);
 			app.UseMvc();
 		}
 	}
