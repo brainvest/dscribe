d807e7f3c92ca5fcd85ee0b080348f016c465ec6
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2018-09-20 15:19:59 +0430

    bring back the authentication from sample server

--- client/demo/src/app/auth/auth.service.ts
+++ client/demo/src/app/auth/auth.service.ts
@@ -40,8 +40,8 @@ export class AuthService {
 
 	getClientSettings(): UserManagerSettings {
 		return {
-			authority: 'https://demo.identityserver.io/',
-			client_id: 'implicit',
+			authority: 'http://localhost:5001/',
+			client_id: 'dscribe',
 			redirect_uri: 'http://localhost:4200/auth-callback',
 			post_logout_redirect_uri: 'http://localhost:4200/',
 			response_type: 'id_token token',
