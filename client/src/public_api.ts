d78639731ef4c3693dcc1efd66e0ce411a6b6df1
Nima Hakimi <nima.hakimi95@gmail.com>
2018-09-20 17:47:06 +0430

    Custom Templating Added card layout and possiblity to add user defined templates for entities to library added a sample in demo project

--- client/src/public_api.ts
+++ client/src/public_api.ts
@@ -2,5 +2,11 @@
  * Public API Surface of src
  */
 
+export * from './lib/list/list-templating/dscribe-template-component';
+export * from './lib/list/list-templating/dscribe-list-template.decorator';
+
+export * from './lib/common/services/metadata.service';
+export * from './lib/common/models/app-instance-info-model';
 export * from './lib/dscribe.component';
 export * from './lib/dscribe.module';
+export * from './lib/dscribe.service';
