69098fae24971d980639675fe9d5ed7546c10084
Nima Hakimi <nima.hakimi95@gmail.com>
2018-09-21 15:17:05 +0430

    added commands

--- client/demo/src/app/app.component.ts
+++ client/demo/src/app/app.component.ts
@@ -1,6 +1,12 @@
 import {Component} from '@angular/core';
 import {AuthService} from './auth/auth.service';
 import {EntityMetadata} from '../../../src/lib/metadata/entity-metadata';
+import {DscribeService} from '../../../src/lib/dscribe.service';
+import {DscribeCommand} from '../../../src/lib/models/dscribe-command';
+import {DscribeFeatureArea} from '../../../src/lib/models/dscribe-feature-area.enum';
+import {DscribeCommandCallbackInput} from '../../../src/lib/models/dscribe-command-callback-input';
+import {ListComponent} from '../../../src/lib/list/list/list.component';
+import {DscribeCommandDisplayPredicate} from '../../../src/lib/models/dscribe-command-display-predicate';
 
 @Component({
 	selector: 'dscribe-host-root',
@@ -10,6 +16,10 @@ import {EntityMetadata} from '../../../src/lib/metadata/entity-metadata';
 export class AppComponent {
 	entity: EntityMetadata;
 
-	constructor(public authService: AuthService) {
+	constructor(public authService: AuthService, private dscribeService: DscribeService) {
+		const clbck = (x: DscribeCommandCallbackInput<ListComponent>) => x.sourceComponent.refreshData();
+		const dispPred = (x: DscribeCommandDisplayPredicate<ListComponent>) => x.component.displayMode === 'grid';
+		this.dscribeService.setCommands([new DscribeCommand(
+			'refresh', 'Refresh', DscribeFeatureArea.Filter, clbck, dispPred)]);
 	}
 }
