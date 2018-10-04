0fd47a743f427f95881b1f42ed1b6694040d1b21
Nima Hakimi <nima.hakimi95@gmail.com>
2018-10-04 11:07:18 +0330

    added some classes to published library

--- client/src/public_api.ts
+++ client/src/public_api.ts
@@ -2,6 +2,15 @@
  * Public API Surface of src
  */
 
+
+
+
+export  * from  './lib/models/dscribe-feature-area.enum';
+export  * from './lib/models/dscribe-command';
+export  * from './lib/models/dscribe-command-display-predicate';
+export * from './lib/models/dscribe-command-callback-input';
+
+export * from './lib/list/list/list.component';
 export * from './lib/list/list-templating/dscribe-template-component';
 export * from './lib/list/list-templating/dscribe-list-template.decorator';
 
@@ -10,3 +19,6 @@ export * from './lib/common/models/app-instance-info-model';
 export * from './lib/dscribe.component';
 export * from './lib/dscribe.module';
 export * from './lib/dscribe.service';
+export * from './lib/metadata/entity-metadata';
+export * from './lib/dscribe-routes';
+
