ecaa3d18c1ca77e0a7d7375130a0806c2a1f91fd
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2018-10-05 21:31:34 +0330

    bring the route info inside DscribeComponent

--- client/src/lib/dscribe.component.ts
+++ client/src/lib/dscribe.component.ts
@@ -1,5 +1,9 @@
 import {Component, Input} from '@angular/core';
 import {DscribeService} from './dscribe.service';
+import {NavigationComponent} from './navigation/navigation.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {ListContainerComponent} from './list/list-container/list-container.component';
+import {MetadataManagementComponent} from './administration/metadata-management/metadata-management.component';
 
 @Component({
 	selector: 'dscribe-root',
@@ -12,6 +16,20 @@ export class DscribeComponent {
 	set authHeaderFetcher(value: () => string) {
 		this.config.authHeaderFetcher = value;
 	}
+
+	static DSCRIBE_ROUTES = [
+		{
+			path: '', component: NavigationComponent,
+			children: [
+				{path: 'main', component: HomePageComponent},
+				{path: 'entity/:entity', component: ListContainerComponent},
+				{path: 'administration', component: MetadataManagementComponent},
+				{path: '', redirectTo: 'main', pathMatch: 'full'}
+			]
+		},
+		{path: '**', redirectTo: 'main'}
+	];
+
 
 	constructor(private config: DscribeService) {
 	}
