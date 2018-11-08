373d6bf547d99e20518c7b00916a194e7723961f
arash bahreini <vbhost.ir@gmail.com>
2018-11-08 13:25:15 +0330

    Working on design app instance management page.

--- server/Core/Runtime/Controllers/AppManagementController.cs
+++ server/Core/Runtime/Controllers/AppManagementController.cs
@@ -1,5 +1,7 @@
 using Brainvest.Dscribe.Abstractions.Models.AppManagement;
 using Brainvest.Dscribe.MetadataDbAccess;
+using Brainvest.Dscribe.MetadataDbAccess.Entities;
+using Brainvest.Dscribe.Runtime.Validations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
@@ -14,6 +16,7 @@ namespace Brainvest.Dscribe.Runtime.Controllers
 	public class AppManagementController : ControllerBase
 	{
 		MetadataDbContext _dbContext;
+
 
 		public AppManagementController(MetadataDbContext dbContext)
 		{
@@ -40,6 +43,140 @@ namespace Brainvest.Dscribe.Runtime.Controllers
 				Title = x.Title,
 				UseUnreleasedMetadata = x.UseUnreleasedMetadata
 			}).ToListAsync();
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> AddAppInstance(AppInstanceInfoModel model)
+		{
+			var validationMessage = await AppManagementValidationLogic.AddAppInstanceValidation(model, _dbContext);
+			if (!string.IsNullOrEmpty(validationMessage))
+			{
+				return StatusCode(400, validationMessage);
+			}
+			_dbContext.AppInstances.Add(new AppInstance
+			{
+				AppTypeId = model.AppTypeId,
+				DatabaseProviderId = model.DatabaseProviderId,
+				GeneratedCodeNamespace = model.GeneratedCodeNamespace,
+				IsEnabled = model.IsEnabled,
+				IsProduction = model.IsProduction,
+				Name = model.Name,
+				Title = model.Title,
+				DataConnectionString = GenerateConnectionString(model.DataConnectionString),
+				MetadataReleaseId = model.MetadataReleaseId,
+
+			});
+			await _dbContext.SaveChangesAsync();
+			return Ok();
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> EditAppInstance(AppInstanceInfoModel model)
+		{
+			var validationMessage = await AppManagementValidationLogic.EditAppInstanceValidation(model, _dbContext);
+			if (!string.IsNullOrEmpty(validationMessage))
+			{
+				return StatusCode(400, validationMessage);
+			}
+
+			var appInstance = await _dbContext.AppInstances.FindAsync(model.Id);
+
+			appInstance.AppTypeId = model.AppTypeId;
+			appInstance.DatabaseProviderId = model.DatabaseProviderId;
+			appInstance.GeneratedCodeNamespace = model.GeneratedCodeNamespace;
+			appInstance.IsEnabled = model.IsEnabled;
+			appInstance.IsProduction = model.IsProduction;
+			appInstance.Name = model.Name;
+			appInstance.Title = model.Title;
+			appInstance.DataConnectionString = GenerateConnectionString(model.DataConnectionString);
+			appInstance.MetadataReleaseId = model.MetadataReleaseId;
+			await _dbContext.SaveChangesAsync();
+			return Ok();
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> DeleteAppInstance(AppInstanceInfoModel model)
+		{
+			var validationMessage = await AppManagementValidationLogic.DeleteAppInstanceValidation(model, _dbContext);
+			if (!string.IsNullOrEmpty(validationMessage))
+			{
+				return StatusCode(400, validationMessage);
+			}
+			var appInstance = await _dbContext.AppInstances.FindAsync(model.Id);
+			_dbContext.AppInstances.Remove(appInstance);
+			await _dbContext.SaveChangesAsync();
+			return Ok();
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<IEnumerable<AppTypeModel>>> GetAppTypes()
+		{
+			return await _dbContext.AppTypes.Select(x =>
+			new AppTypeModel
+			{
+				Id = x.Id,
+				Name = x.Name,
+				Title = x.Title
+			}).ToListAsync();
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> AddAppType(AppTypeModel model)
+		{
+			var validationMessage = await AppManagementValidationLogic.AddAppTypeValidation(model, _dbContext);
+			if (!string.IsNullOrEmpty(validationMessage))
+			{
+				return StatusCode(400, validationMessage);
+			}
+			_dbContext.AppTypes.Add(new AppType
+			{
+				Title = model.Title,
+				Name = model.Name,
+				Id = model.Id
+			});
+			await _dbContext.SaveChangesAsync();
+			return Ok();
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> EditAppType(AppTypeModel model)
+		{
+			var validationMessage = await AppManagementValidationLogic.EditAppTypeValidation(model, _dbContext);
+			if (!string.IsNullOrEmpty(validationMessage))
+			{
+				return StatusCode(400, validationMessage);
+			}
+			var appType = await _dbContext.AppTypes.FindAsync(model.Id);
+
+			appType.Name = model.Name;
+			appType.Title = model.Title;
+
+			await _dbContext.SaveChangesAsync();
+			return Ok();
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> DeleteAppType(AppTypeModel model)
+		{
+			var validationMessage = await AppManagementValidationLogic.DeleteAppTypeValidation(model, _dbContext);
+			if (!string.IsNullOrEmpty(validationMessage))
+			{
+				return StatusCode(400, validationMessage);
+			}
+			var appType = await _dbContext.AppTypes.FindAsync(model.Id);
+			_dbContext.AppTypes.Remove(appType);
+			await _dbContext.SaveChangesAsync();
+			return Ok();
+		}
+
+		public string GenerateConnectionString(DataConnectionStringModel model)
+		{
+			return model.Server + ";" +
+					model.Database + ";" +
+					model.Trusted_Connection + ";" +
+					model.MultipleActiveResultSets + ";" +
+					model.User + ";" +
+					model.Password;
 		}
 	}
 }
---
+++ server/Core/Runtime/Validations/AppManagementValidationLogic.cs
@@ -0,0 +1,47 @@
+using Brainvest.Dscribe.Abstractions.Models.AppManagement;
+using Brainvest.Dscribe.MetadataDbAccess;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brainvest.Dscribe.Runtime.Validations
+{
+	public static class AppManagementValidationLogic
+	{
+		public static async Task<string> AddAppInstanceValidation(AppInstanceInfoModel model, MetadataDbContext dbContext)
+		{
+			if (await dbContext.AppInstances.AnyAsync(x => x.Name == model.Name))
+			{
+				return "App instance name " + model.Name + " is already exist.";
+			}
+			return string.Empty;
+		}
+
+		public static async Task<string> EditAppInstanceValidation(AppInstanceInfoModel model, MetadataDbContext dbContext)
+		{
+			return await Task.FromResult(string.Empty);
+		}
+
+		public static async Task<string> DeleteAppInstanceValidation(AppInstanceInfoModel model, MetadataDbContext dbContext)
+		{
+			return await Task.FromResult(string.Empty);
+		}
+
+		public static async Task<string> AddAppTypeValidation(AppTypeModel model, MetadataDbContext dbContext)
+		{
+			return await Task.FromResult(string.Empty);
+		}
+
+		public static async Task<string> EditAppTypeValidation(AppTypeModel model, MetadataDbContext dbContext)
+		{
+			return await Task.FromResult(string.Empty);
+		}
+
+		public static async Task<string> DeleteAppTypeValidation(AppTypeModel model, MetadataDbContext dbContext)
+		{
+			return await Task.FromResult(string.Empty);
+		}
+	}
+}
