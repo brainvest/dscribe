bdfd35ec16a55212a54783e4c0e21706f590202d
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2018-11-30 00:34:36 +0330

    Move ReportFormat and ReportDefinition to Metadata

--- server/Core/MetadataDbAccess/Entities/Reporting/ReportFormat.cs
+++ server/Core/MetadataDbAccess/Entities/Reporting/ReportFormat.cs
@@ -2,7 +2,7 @@ namespace Brainvest.Dscribe.MetadataDbAccess.Entities.Reporting
 {
 	public class ReportFormat
 	{
-		public ReportFormats Id { get; set; }
+		public ReportFormat Id { get; set; }
 		public string Name { get; set; }
 		public string Title { get; set; }
 	}
