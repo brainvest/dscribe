ebee842223b01e548b12ada26791dbbde9b09391
Arash <vbhost.ir@gmail.com>
2019-01-03 14:43:11 +0330

    Design DataLog Middleware.

--- server/Infrastructure/Abstractions/Models/DataRequestAction.cs
+++ server/Infrastructure/Abstractions/Models/DataRequestAction.cs
@@ -6,12 +6,9 @@ namespace Brainvest.Dscribe.Abstractions.Models
 {
 	public enum DataRequestAction
 	{
-		Detached = 0,
-		Unchanged = 1,
-		Deleted = 2,
-		Modified = 3,
-		Added = 4,
-		Listed = 5,
-		None = 6
+		Add = 1,
+		Edit = 2,
+		List = 3,
+		Delete = 4
 	}
 }
