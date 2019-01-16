85b766b42ef82b360665e4b05d878ec03f0c8c84
Arash <vbhost.ir@gmail.com>
2019-01-16 12:49:17 +0330

    Done with adding EntityId and DataId into the DataLog table. (#120)

--- server/Infrastructure/Abstractions/IDataLogImplementation.cs
+++ server/Infrastructure/Abstractions/IDataLogImplementation.cs
@@ -1,13 +1,9 @@
-using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Brainvest.Dscribe.Abstractions
 {
 	public interface IDataLogImplementation
 	{
-		Task SaveDataChanges(object businessRepository);
+		Task SaveDataChanges(object businessRepository,string entityTypeName);
 	}
 }
--- server/Infrastructure/LobTools/DataLog/DataLogBusiness.cs
+++ server/Infrastructure/LobTools/DataLog/DataLogBusiness.cs
@@ -1,6 +1,9 @@
 using Brainvest.Dscribe.Abstractions;
 using Brainvest.Dscribe.Abstractions.Models;
 using Brainvest.Dscribe.LobTools.Entities;
+using Brainvest.Dscribe.LobTools.Models;
+using Brainvest.Dscribe.MetadataDbAccess;
+using Brainvest.Dscribe.MetadataDbAccess.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
@@ -16,14 +19,26 @@ namespace Brainvest.Dscribe.LobTools.DataLog
 	{
 		private readonly LobToolsDbContext _lobToolsDbContext;
 		private readonly IHttpContextAccessor _httpContextAccessor;
+		private readonly MetadataDbContext _metadataDbContext;
 
-		public DataLogBusiness(LobToolsDbContext lobToolsDbContext, IHttpContextAccessor httpContextAccessor)
+		public DataLogBusiness(
+			LobToolsDbContext lobToolsDbContext,
+			IHttpContextAccessor httpContextAccessor,
+			MetadataDbContext metadataDbContext)
 		{
 			_lobToolsDbContext = lobToolsDbContext;
 			_httpContextAccessor = httpContextAccessor;
+			_metadataDbContext = metadataDbContext;
+
 		}
-		public async Task SaveDataChanges(object businessRepository)
+		public async Task SaveDataChanges(object businessRepository, string entityTypeName)
 		{
+			var entityType = await _metadataDbContext.EntityTypes
+				.Where(x => x.Name == entityTypeName)
+				.Include(x => x.Properties)
+				.Include("Properties.GeneralUsageCategory")
+				.FirstOrDefaultAsync();
+
 			var dataChanges = (businessRepository as DbContext).ChangeTracker
 				.Entries()
 				.Where(x => x.State == EntityState.Modified)
@@ -35,8 +50,14 @@ namespace Brainvest.Dscribe.LobTools.DataLog
 				.ToList();
 			foreach (var dataChange in dataChanges)
 			{
+				// Note. This history does not support primary kyes which are not int E.g. GUID, String
+				var primaryKey = entityType.Properties.Where(x => x.GeneralUsageCategory.Name == "PrimaryKey").FirstOrDefault();
+
 				var data = new Entities.DataLog
 				{
+					EntityId = entityType.Id,
+					// Consider that data always has primary key
+					DataId = Convert.ToInt64(dataChange.Entity.GetType().GetProperty(primaryKey.Name).GetValue(dataChange.Entity)),
 					Body = JsonConvert.SerializeObject(dataChange.Entity),
 					DataRequestAction = (DataRequestAction)dataChange.Action,
 					RequestLogId = ((RequestLogModel)_httpContextAccessor.HttpContext.Items["RequestLog"]).Id
