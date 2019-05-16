a7a7fe187c31e422b1571b582e09e46db3ccbe45
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2019-05-16 23:13:47 +0430

    update packages

--- server/Infrastructure/SampleAuthServer/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ server/Infrastructure/SampleAuthServer/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -11,34 +11,47 @@ using Microsoft.Extensions.Logging;
 
 namespace Brainvest.Dscribe.Infrastructure.SampleAuthServer.Areas.Identity.Pages.Account
 {
-    [AllowAnonymous]
-    public class LogoutModel : PageModel
-    {
-        private readonly SignInManager<User> _signInManager;
-        private readonly ILogger<LogoutModel> _logger;
+	[AllowAnonymous]
+	public class LogoutModel : PageModel
+	{
+		private readonly SignInManager<User> _signInManager;
+		private readonly ILogger<LogoutModel> _logger;
 
-        public LogoutModel(SignInManager<User> signInManager, ILogger<LogoutModel> logger)
-        {
-            _signInManager = signInManager;
-            _logger = logger;
-        }
+		public LogoutModel(SignInManager<User> signInManager, ILogger<LogoutModel> logger)
+		{
+			_signInManager = signInManager;
+			_logger = logger;
+		}
 
-        public void OnGet()
-        {
-        }
+		public async Task<IActionResult> OnGet(string returnUrl = null)
+		{
+			if (_signInManager.IsSignedIn(User))
+			{
+				await _signInManager.SignOutAsync();
+				return RedirectToPage(new { returnUrl = returnUrl });
+			}
+			if (returnUrl != null)
+			{
+				return LocalRedirect(returnUrl);
+			}
+			else
+			{
+				return Page();
+			}
+		}
 
-        public async Task<IActionResult> OnPost(string returnUrl = null)
-        {
-            await _signInManager.SignOutAsync();
-            _logger.LogInformation("User logged out.");
-            if (returnUrl != null)
-            {
-                return LocalRedirect(returnUrl);
-            }
-            else
-            {
-                return Page();
-            }
-        }
-    }
+		public async Task<IActionResult> OnPost(string returnUrl = null)
+		{
+			await _signInManager.SignOutAsync();
+			_logger.LogInformation("User logged out.");
+			if (returnUrl != null)
+			{
+				return LocalRedirect(returnUrl);
+			}
+			else
+			{
+				return Page();
+			}
+		}
+	}
 }
