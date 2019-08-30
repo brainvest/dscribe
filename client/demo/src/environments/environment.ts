cbe5f8d90a7f0d53cee2ed70bdb3a7b39a3f3024
Nima <nima.hakimi95@gmail.com>
2019-08-30 11:25:16 +0430

    Imp: changed ui for auth pages

--- client/demo/src/environments/environment.ts
+++ client/demo/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
 	production: false,
 	apiServerRoot: 'https://my.node-4.com/',
 	auth: {
-		authority: 'https://my.node-4.com/auth/',
+		authority: 'http://localhost:5001/auth/',
 		client_id: 'invoice',
 		redirect_uri: 'http://localhost:4200/auth-callback',
 		post_logout_redirect_uri: 'http://localhost:4200/',
