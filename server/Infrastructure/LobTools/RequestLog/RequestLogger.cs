8d014757898b38c1a8aa82f57c63da0515aa170e
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2019-09-03 23:36:35 +0430

    fix request logging in some cases

--- server/Infrastructure/LobTools/RequestLog/RequestLogger.cs
+++ server/Infrastructure/LobTools/RequestLog/RequestLogger.cs
@@ -3,6 +3,7 @@ using Brainvest.Dscribe.Abstractions.Helpers;
 using Brainvest.Dscribe.Abstractions.Models;
 using Brainvest.Dscribe.LobTools.Entities;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using MiddleWare.Log;
 using System;
 using System.Diagnostics;
@@ -17,11 +18,13 @@ namespace Brainvest.Dscribe.LobTools.RequestLog
 		private IImplementationsContainer _implementationsContainer;
 		private LobToolsDbContext _dbContext;
 		private Stopwatch _stopwatch = new Stopwatch();
+		private readonly ILogger<RequestLogger> _logger;
 
-		public RequestLogger(IImplementationsContainer implementationsContainer, IHttpContextAccessor httpContextAccessor)
+		public RequestLogger(IImplementationsContainer implementationsContainer, IHttpContextAccessor httpContextAccessor, ILogger<RequestLogger> logger)
 		{
 			_implementationsContainer = implementationsContainer;
 			_dbContext = _implementationsContainer.GetLobDbContext<LobToolsDbContext>(httpContextAccessor.HttpContext);
+			_logger = logger;
 		}
 
 		public async Task<RequestLogModel> RequestIndiactor(HttpContext httpContext)
@@ -39,15 +42,19 @@ namespace Brainvest.Dscribe.LobTools.RequestLog
 				StartTime = DateTime.Now,
 			};
 
-			#region Get Request Body
 			var timer = Stopwatch.StartNew();
-			request.Body = await new StreamReader(httpContext.Request.Body).ReadToEndAsync();
-			var injectedRequestStream = new MemoryStream();
-			var bytesToWrite = Encoding.UTF8.GetBytes(request.Body);
-			injectedRequestStream.Write(bytesToWrite, 0, bytesToWrite.Length);
-			injectedRequestStream.Seek(0, SeekOrigin.Begin);
-			httpContext.Request.Body = injectedRequestStream;
-			#endregion
+			if (httpContext.Request?.Body != null && !string.IsNullOrWhiteSpace(httpContext.Request.ContentType) &&
+				!httpContext.Request.ContentType.StartsWith("multipart/form-data", StringComparison.InvariantCultureIgnoreCase))
+			{
+				#region Get Request Body
+				request.Body = await new StreamReader(httpContext.Request.Body).ReadToEndAsync();
+				var injectedRequestStream = new MemoryStream();
+				var bytesToWrite = Encoding.UTF8.GetBytes(request.Body);
+				injectedRequestStream.Write(bytesToWrite, 0, bytesToWrite.Length);
+				injectedRequestStream.Seek(0, SeekOrigin.Begin);
+				httpContext.Request.Body = injectedRequestStream;
+				#endregion
+			}
 
 			request.RequestSize = httpContext.Request.ContentLength;
 
@@ -81,15 +88,23 @@ namespace Brainvest.Dscribe.LobTools.RequestLog
 
 		public async Task ExceptionIndiactor(HttpContext httpContext, RequestLogModel requestLog, Exception ex)
 		{
-			var request = await _dbContext.RequestLogs.FindAsync(requestLog.Id);
-			request.ResponseStatusCode = httpContext.Response.StatusCode;
-			request.ExceptionMessage = ex.GetFullMessage();
-			request.ExceptionTitle = ex.Message;
-			request.HadException = true;
-			request.Failed = true;
-			_stopwatch.Stop();
-			request.ProcessDuration = _stopwatch.Elapsed.TotalSeconds;
-			await _dbContext.SaveChangesAsync();
+			try
+			{
+				var request = await _dbContext.RequestLogs.FindAsync(requestLog.Id);
+				request.ResponseStatusCode = httpContext.Response.StatusCode;
+				request.ExceptionMessage = ex.GetFullMessage();
+				request.ExceptionTitle = ex.Message;
+				request.HadException = true;
+				request.Failed = true;
+				_stopwatch.Stop();
+				request.ProcessDuration = _stopwatch.Elapsed.TotalSeconds;
+				await _dbContext.SaveChangesAsync();
+			}
+			catch (Exception loggingException)
+			{
+				_logger.LogError(ex, "An error occured while processing the request.");
+				_logger.LogError(loggingException, "Additionally, an error occured while logging it to database.");
+			}
 		}
 
 		public void Dispose()
