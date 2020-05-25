1deb1bfa43fe9638c740fbed5743a38a432889ff
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2020-05-25 10:59:41 +0800

    migrations, unique index for users

---
+++ server/Migrations/Migrations_Runtime_MySql/Migrations/MetadataDbContext_MySqlMigrations/20200519160040_UniqueIndexForUsers.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Migrations_Runtime_MySql.Migrations.MetadataDbContext_MySqlMigrations
+{
+    public partial class UniqueIndexForUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UnifiedExternalUserId",
+                table: "users",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_UnifiedExternalUserId",
+                table: "users",
+                column: "UnifiedExternalUserId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_users_UnifiedExternalUserId",
+                table: "users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UnifiedExternalUserId",
+                table: "users",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
