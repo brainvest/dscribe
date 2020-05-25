2fc9210085bc6b7cd1dcba87c5d9eae485e4009f
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2020-05-25 13:13:08 +0800

    migration, remove connectionstrings from database

---
+++ server/Migrations/Migrations_Runtime_MySql/Migrations/MetadataDbContext_MySqlMigrations/20200525034831_RemoveConnectionStringsFromDatabase.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Migrations_Runtime_MySql.Migrations.MetadataDbContext_MySqlMigrations
+{
+    public partial class RemoveConnectionStringsFromDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataConnectionString",
+                table: "appinstances");
+
+            migrationBuilder.DropColumn(
+                name: "LobConnectionString",
+                table: "appinstances");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DataConnectionStringTemplateName",
+                table: "appinstances",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LoadBusinessFromAssemblyName",
+                table: "appinstances",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LobConnectionStringTemplateName",
+                table: "appinstances",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LobDatabaseName",
+                table: "appinstances",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MainDatabaseName",
+                table: "appinstances",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataConnectionStringTemplateName",
+                table: "appinstances");
+
+            migrationBuilder.DropColumn(
+                name: "LoadBusinessFromAssemblyName",
+                table: "appinstances");
+
+            migrationBuilder.DropColumn(
+                name: "LobConnectionStringTemplateName",
+                table: "appinstances");
+
+            migrationBuilder.DropColumn(
+                name: "LobDatabaseName",
+                table: "appinstances");
+
+            migrationBuilder.DropColumn(
+                name: "MainDatabaseName",
+                table: "appinstances");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DataConnectionString",
+                table: "appinstances",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LobConnectionString",
+                table: "appinstances",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+    }
+}
