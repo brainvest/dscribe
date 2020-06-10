851be7c4f3b61f430d346a5066607fd68e13ffed
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2020-06-10 17:49:49 +0800

    add metadata migrations for postgresql

--- server/Migrations/Migrations_Runtime_PostgreSql/Migrations/20200610094844_RemoveConnectionStringsFromDatabase.Designer.cs
+++ server/Migrations/Migrations_Runtime_PostgreSql/Migrations/20200610094844_RemoveConnectionStringsFromDatabase.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Migrations_Runtime_PostgreSql;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -9,9 +10,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Migrations_Runtime_PostgreSql.Migrations
 {
     [DbContext(typeof(MetadataDbContext_PostgreSql))]
-    partial class MetadataDbContext_PostgreSqlModelSnapshot : ModelSnapshot
+    [Migration("20200610094844_RemoveConnectionStringsFromDatabase")]
+    partial class RemoveConnectionStringsFromDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ server/Migrations/Migrations_Runtime_PostgreSql/Migrations/20200610094844_RemoveConnectionStringsFromDatabase.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Migrations_Runtime_PostgreSql.Migrations
+{
+    public partial class RemoveConnectionStringsFromDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataConnectionString",
+                table: "AppInstances");
+
+            migrationBuilder.DropColumn(
+                name: "LobConnectionString",
+                table: "AppInstances");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DataConnectionStringTemplateName",
+                table: "AppInstances",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LobConnectionStringTemplateName",
+                table: "AppInstances",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LobDatabaseName",
+                table: "AppInstances",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MainDatabaseName",
+                table: "AppInstances",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UnifiedExternalUserId",
+                table: "Users",
+                column: "UnifiedExternalUserId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_UnifiedExternalUserId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DataConnectionStringTemplateName",
+                table: "AppInstances");
+
+            migrationBuilder.DropColumn(
+                name: "LobConnectionStringTemplateName",
+                table: "AppInstances");
+
+            migrationBuilder.DropColumn(
+                name: "LobDatabaseName",
+                table: "AppInstances");
+
+            migrationBuilder.DropColumn(
+                name: "MainDatabaseName",
+                table: "AppInstances");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DataConnectionString",
+                table: "AppInstances",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LobConnectionString",
+                table: "AppInstances",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
